aecf954d49cecff6d8aebdbf86b73e955615222c
PsychoGas <psychogas69@gmail.com>
2025-06-11 18:46:29 +0000

    title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'EMC SquidGame Eliminations',
+  description: 'Created with love by EMC Team',
+  generator: 'EMC',
 }
 
 export default function RootLayout({
